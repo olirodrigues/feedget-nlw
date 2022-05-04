242eb78545c1b38970d140a585b84d98b3f4af80
olirodrigues <olivia.rt@gmail.com>
2022-05-04 10:32:29 -0300

    Adicionado tela de sucesso de feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -7,11 +7,13 @@ import { ScreenshotButton } from "../ScreenshotButton";
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
   onFeedbackRestartRequested: () => void;
+  onFeedbackSent: () => void;
 }
 
 export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested,
+  onFeedbackSent,
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState("");
@@ -25,6 +27,8 @@ export function FeedbackContentStep({
       screenshot,
       comment,
     });
+
+    onFeedbackSent();
   }
 
   return (
@@ -51,7 +55,7 @@ export function FeedbackContentStep({
 
       <form onSubmit={handleSubmitFeedback} className='my-4 w-full'>
         <textarea
-          className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
+          className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-700 focus:ring-brand-500 resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
           placeholder='Conte com detalhes o que está acontecendo...'
           onChange={(event) => setComment(event.target.value)}
         />
